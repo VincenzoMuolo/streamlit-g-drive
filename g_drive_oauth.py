dfdfe399d132b164652f40c6d4916e6993063581
Vincenzo Muolo <vincenzo.muolo96@gmail.com>
2025-02-20 14:39:11 +0100

    requirement.txt added

--- g_drive_oauth.py
+++ g_drive_oauth.py
@@ -1,5 +1,6 @@
 import os
 import tomllib
+import streamlit as st
 
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
@@ -8,18 +9,15 @@ class GoogleDriveService:
     def __init__(self):
         self._SCOPES = ['https://www.googleapis.com/auth/drive']
 
-        # Ottieni la directory corrente del file Python
-        _base_path = os.path.dirname(__file__)  
+        # Accesso alle credenziali di accesso in locale
+        #_base_path = os.path.dirname(__file__)  
+        #_secrets_path = os.path.join(_base_path, ".streamlit", "secrets.toml")  
+        #with open(_secrets_path, "rb") as f:
+        #    secrets = tomllib.load(f)  
+        #self._credentials_dict = secrets["google_drive_API_service_account"]  
         
-        # Percorso corretto del file secrets.toml nella stessa directory del file Python
-        _secrets_path = os.path.join(_base_path, ".streamlit", "secrets.toml")  
-
-        # Apri il file TOML correttamente
-        with open(_secrets_path, "rb") as f:
-            secrets = tomllib.load(f)  
-
-        # Salva le credenziali
-        self._credentials_dict = secrets["google_drive_API_service_account"]  
+        # Accesso alle credenziali da Streamlit cloud
+        self._credentials_dict = st.secrets["google_drive_API_service_account"] 
 
     def build(self):
         creds = ServiceAccountCredentials.from_json_keyfile_dict(self._credentials_dict, self._SCOPES)
